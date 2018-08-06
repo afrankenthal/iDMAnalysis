efe631a80e261719f7b7892eed999ca0c64f3021
Weinan Si <weinan.si@cern.ch>
2018-08-06 12:03:05 -0500

    customize tuplizer config with year

--- washAOD/python/tuplizer_cfg.py
+++ washAOD/python/tuplizer_cfg.py
@@ -86,6 +86,9 @@ process.RECO_gbm = recoEffiForMuTrack.clone(muTrack = cms.InputTag("globalMuons"
 
 ## trigger efficiency in terms of events
 from Firefighter.washAOD.trigEffiForMuTrack_cfi import trigEffiForMuTrack
+### 2017
+process.TRIG_dsa = trigEffiForMuTrack.clone(trigPath = cms.string('HLT_TrkMu16_DoubleTrkMu6NoFiltersNoVtx'))
+### 2018
 process.TRIG_dsa_DL2Mu23               = trigEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha'))
 process.TRIG_dsa_DL2Mu23NoL2           = trigEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha_NoL2Matched'))
 process.TRIG_dsa_DL2Mu23CosmicSeed     = trigEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed'))
@@ -93,6 +96,9 @@ process.TRIG_dsa_DL2Mu23CosmicSeedNoL2 = trigEffiForMuTrack.clone(trigPath = cms
 
 ## trigger efficiency itself
 from Firefighter.washAOD.trigSelfEffiForMuTrack_cfi import trigSelfEffiForMuTrack
+### 2017
+process.TRIGself_dsa = trigSelfEffiForMuTrack.clone(trigPath = cms.string('HLT_TrkMu16_DoubleTrkMu6NoFiltersNoVtx'), nMuons = cms.int32(3))
+### 2018
 process.TRIGself_dsa_DL2Mu23               = trigSelfEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha'))
 process.TRIGself_dsa_DL2Mu23NoL2           = trigSelfEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha_NoL2Matched'))
 process.TRIGself_dsa_DL2Mu23CosmicSeed     = trigSelfEffiForMuTrack.clone(trigPath = cms.string('HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed'))
@@ -100,17 +106,28 @@ process.TRIGself_dsa_DL2Mu23CosmicSeedNoL2 = trigSelfEffiForMuTrack.clone(trigPa
 
 
 ## constructing the path
-process.p = cms.Path(process.GEN
-                     + process.RECO_dsa
-                     + process.RECO_dgm
-                     + process.RECO_rsa
-                     + process.RECO_gbm
-                     + process.TRIG_dsa_DL2Mu23
-                     + process.TRIG_dsa_DL2Mu23NoL2
-                     + process.TRIG_dsa_DL2Mu23CosmicSeed
-                     + process.TRIG_dsa_DL2Mu23CosmicSeedNoL2
-                     + process.TRIGself_dsa_DL2Mu23
-                     + process.TRIGself_dsa_DL2Mu23NoL2
-                     + process.TRIGself_dsa_DL2Mu23CosmicSeed
-                     + process.TRIGself_dsa_DL2Mu23CosmicSeedNoL2
-                     )
+if options.year == 2017:
+    process.p = cms.Path(process.GEN
+                         + process.RECO_dsa
+                         + process.RECO_dgm
+                         + process.RECO_rsa
+                         + process.RECO_gbm
+                         + process.TRIG_dsa
+                         + process.TRIGself_dsa
+                         )
+
+if options.year == 2018:
+    process.p = cms.Path(process.GEN
+                         + process.RECO_dsa
+                         + process.RECO_dgm
+                         + process.RECO_rsa
+                         + process.RECO_gbm
+                         + process.TRIG_dsa_DL2Mu23
+                         + process.TRIG_dsa_DL2Mu23NoL2
+                         + process.TRIG_dsa_DL2Mu23CosmicSeed
+                         + process.TRIG_dsa_DL2Mu23CosmicSeedNoL2
+                         + process.TRIGself_dsa_DL2Mu23
+                         + process.TRIGself_dsa_DL2Mu23NoL2
+                         + process.TRIGself_dsa_DL2Mu23CosmicSeed
+                         + process.TRIGself_dsa_DL2Mu23CosmicSeedNoL2
+                         )
--- washAOD/test/mkNtuple.sh
+++ washAOD/test/mkNtuple.sh
@@ -3,6 +3,6 @@
 TAG=$1
 DATALIST=${TAG}.list
 OUTPUT=${TAG}.root
-YEAR=2018
+YEAR=2017
 
 cmsRun ../python/tuplizer_cfg.py year=${YEAR} inputFiles_load=../data/${YEAR}/${DATALIST} outputFile=${YEAR}/${OUTPUT} 2>&1
