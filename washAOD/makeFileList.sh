9781caf5d0bd92400d006f9c8d8c1a6142621f4a
Andre Frankenthal <as2872@cornell.edu>
2019-02-07 19:10:13 -0600

    Whoops, forgot to add these two files to TrigRecoEffi commit, don't want to git rebase to not confuse Tres's history

--- washAOD/makeFileList.sh
+++ washAOD/makeFileList.sh
@@ -1,13 +1,41 @@
 #! /bin/bash
 
-mchi=52p5
-dmchi=5p0
-ctau=1
+#### THIS SCRIPT NEEDS TO BE SOURCED!
+#### (due to use of eosls command)
+
+#### Choose lpcuser and comment out the "input mode" 
+#### you don't want to use
+
+lpcuser=as2872
 basedir="2018/GenFilter"
-basename="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
 
-eos root://cmseos.fnal.gov ls /store/user/as2872/iDM/AOD_Samples/$basedir/$basename > data/iDM/$basedir/${basename}.list
+# Use single mass and ctau parameter
+mchi=6p0
+dmchi=2p0
+ctau=10
+dirs="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
 
-sed -i '/MINIAOD/d' data/iDM/$basedir/${basename}.list
+# OR use directories in EOS to get all samples (uncomment)
+#dirs=$(eosls /store/user/$lpcuser/iDM/AOD_Samples/2018/GenFilter/)
 
-sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/asterenb/iDM/AOD_Samples/$basedir/$basename/|" data/iDM/$basedir/${basename}.list
+#for dir in $dirs; do 
+#    echo $dir
+#    mchistring="$(cut -d'_' -f1 <<< $dir)"
+#    mchi="$(cut -d'-' -f2 <<< $mchistring)"
+#    echo $mchi
+#done
+
+for dir in $dirs; do
+    echo "Processing $dir"
+    eos root://cmseos.fnal.gov ls /store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir > data/iDM/$basedir/${dir}.list
+    sed -i '/MINIAOD/d' data/iDM/$basedir/${dir}.list
+    sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir/|" data/iDM/$basedir/${dir}.list
+done
+
+#basename="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
+#
+#eos root://cmseos.fnal.gov ls /store/user/as2872/iDM/AOD_Samples/$basedir/$basename > data/iDM/$basedir/${basename}.list
+#
+#sed -i '/MINIAOD/d' data/iDM/$basedir/${basename}.list
+#
+#sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/asterenb/iDM/AOD_Samples/$basedir/$basename/|" data/iDM/$basedir/${basename}.list
--- washAOD/plugins/SealModule.cc
+++ washAOD/plugins/SealModule.cc
@@ -6,9 +6,11 @@
 #include "Firefighter/washAOD/interface/trigEffiForMuTrack.h"
 #include "Firefighter/washAOD/interface/trigSelfEffiForMuTrack.h"
 #include "Firefighter/washAOD/interface/SignalRegionEffi.h"
+#include "Firefighter/washAOD/interface/TrigRecoEffi.h"
 
 DEFINE_FWK_MODULE(genTuplizer);
 DEFINE_FWK_MODULE(recoEffiForMuTrack);
 DEFINE_FWK_MODULE(trigEffiForMuTrack);
 DEFINE_FWK_MODULE(trigSelfEffiForMuTrack);
 DEFINE_FWK_MODULE(SignalRegionEffi);
+DEFINE_FWK_MODULE(TrigRecoEffi);
