c105bebd11e69908699ff49640a210d12a0cde84
Andre Frankenthal <as2872@cornell.edu>
2019-03-04 15:16:01 -0600

    Added crab and multicrab scripts

---
+++ washAOD/mergeCRABOutput.py
@@ -0,0 +1,25 @@
+## Run with python mergeCRABOutput.py, instead of ./mergeCRABOutput.py
+## This guarantees the right python from the CMSSW release is called
+
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.UserUtilities import setConsoleLogLevel
+from CRABClient.ClientUtilities import LOGLEVEL_MUTE
+setConsoleLogLevel(LOGLEVEL_MUTE)
+import sys
+
+# import os
+# cwd = os.getcwd()
+# print "Current directory: ", cwd
+
+if len(sys.argv) > 1:
+    res = crabCommand('status', dir = sys.argv[1])
+# else:
+    # res = crabCommand('status')
+
+#if res['status'] == 'COMPLETED':
+userWebDirURL = res['userWebDirURL']
+datestamp = userWebDirURL.split(':')[-2].split('/')[-1]
+print datestamp
+    #exit(0)
+# else:
+    # exit(1)
