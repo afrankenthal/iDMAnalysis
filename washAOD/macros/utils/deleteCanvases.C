279361af8b06d8dbd368f55a6b4a393f02111b37
Andre Frankenthal <as2872@cornell.edu>
2019-09-14 23:13:49 -0500

    New macro to delete all canvases from ROOT files

---
+++ washAOD/macros/utils/deleteCanvases.C
@@ -0,0 +1,26 @@
+void deleteCanvases() {
+
+    TFile * file = new TFile("plots_CR_dR.root", "UPDATE");
+    
+    TIter next((TList*)file->GetListOfKeys());
+    TKey * key;
+    key = (TKey*)next();
+    while (1) {
+        cout << "class name " << key->GetClassName() << endl;
+        cout << " name " << key->GetName() << endl;
+        if (TString(key->GetClassName()) != TString("TCanvas")) {
+            key = (TKey*)next();
+            continue;
+        }
+        //TCanvas * c = (TCanvas*)key->ReadObj();
+        key->Delete();
+        delete key;
+        key = (TKey*)next();
+        //c->SetDirectory(0);
+        //file->Delete(Form("%s;*", c->GetName()));
+        //delete c;
+        if (!key) break;
+    }
+    file->Write();
+    file->Close();
+}
