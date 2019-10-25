da9e7558a6280c8f7dd810f69ded693394aae8c1
tres <tres.reid@gmail.com>
2019-10-25 09:30:17 -0500

    push mettrig

--- washAOD/interface/MetTrigSelfEffiForMuTrack.h
+++ washAOD/interface/MetTrigSelfEffiForMuTrack.h
@@ -9,6 +9,9 @@
  * Check trigger firing condition
  */
 
+#include <boost/format.hpp>
+#include <boost/any.hpp>
+
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -57,14 +60,25 @@ class MetTrigSelfEffiForMuTrack : public edm::one::EDAnalyzer<edm::one::WatchRun
 
         const edm::InputTag muTrackTag_;
 	const edm::InputTag genEvtInfoTag_;
-//        const edm::InputTag genParticleTag_;
-//       const edm::InputTag genJetTag_;
-//        const edm::InputTag genMetTag_;
+ 
+
+        const edm::InputTag genParticleTag_;
+       const edm::InputTag genJetTag_;
+        const edm::InputTag genMetTag_;
+
         const edm::InputTag caloMetTag_;
         const edm::InputTag recoMetTag_;
         const edm::InputTag recoJetTag_;
         const edm::InputTag trigResultsTag_;
         const edm::InputTag trigEventTag_;
+ const edm::InputTag HBHENoiseFilterResultProducerTag_;
+        const edm::InputTag primaryVertexFilterTag_;
+        const edm::InputTag globalSuperTightHalo2016FilterTag_;
+        const edm::InputTag EcalDeadCellTriggerPrimitiveFilterTag_;
+        const edm::InputTag ecalBadCalibFilterTag_;
+        const edm::InputTag BadPFMuonFilterTag_;
+        const edm::InputTag muonBadTrackFilterTag_;
+
         const std::string trigPathNoVer_;
         const std::string trigPathNoVer0_;
         const std::string processName_;
@@ -72,28 +86,44 @@ class MetTrigSelfEffiForMuTrack : public edm::one::EDAnalyzer<edm::one::WatchRun
         const edm::EDGetTokenT<reco::TrackCollection> muTrackToken_;
 	const edm::EDGetTokenT<GenEventInfoProduct> genEvtInfoToken_;
         const edm::EDGetTokenT<reco::GenParticleCollection> genParticleToken_;
-//        const edm::EDGetTokenT<reco::GenJetCollection> genJetToken_;
-//        const edm::EDGetTokenT<reco::GenMETCollection> genMetToken_;
+
+        const edm::EDGetTokenT<reco::GenJetCollection> genJetToken_;
+        const edm::EDGetTokenT<reco::GenMETCollection> genMetToken_;
+
         const edm::EDGetTokenT<reco::CaloMETCollection> caloMetToken_;
         const edm::EDGetTokenT<reco::PFMETCollection> recoMetToken_;
         const edm::EDGetTokenT<reco::PFJetCollection> recoJetToken_;
         const edm::EDGetTokenT<edm::TriggerResults> trigResultsToken_;
         const edm::EDGetTokenT<trigger::TriggerEvent> trigEventToken_;
-	
+	 const edm::EDGetTokenT<bool>HBHENoiseFilterResultProducerToken_;
+        const edm::EDGetTokenT<int>primaryVertexFilterToken_;
+        const edm::EDGetTokenT<bool>globalSuperTightHalo2016FilterToken_;
+        const edm::EDGetTokenT<bool>EcalDeadCellTriggerPrimitiveFilterToken_;
+        const edm::EDGetTokenT<bool>ecalBadCalibFilterToken_;
+        const edm::EDGetTokenT<bool>BadPFMuonFilterToken_;
+        const edm::EDGetTokenT<bool>muonBadTrackFilterToken_;
 
 
         edm::Service<TFileService> fs;
         edm::Handle<reco::TrackCollection> muTrackHandle_;
 	edm::Handle<GenEventInfoProduct> genEvtInfoHandle_;
-//        edm::Handle<reco::GenParticleCollection> genParticleHandle_;
-//        edm::Handle<reco::GenJetCollection> genJetHandle_;
-//        edm::Handle<reco::GenMETCollection> genMetHandle_;
+        
+        edm::Handle<reco::GenParticleCollection> genParticleHandle_;
+        edm::Handle<reco::GenJetCollection> genJetHandle_;
+        edm::Handle<reco::GenMETCollection> genMetHandle_;
+        
         edm::Handle<reco::CaloMETCollection> caloMetHandle_;
         edm::Handle<reco::PFMETCollection> recoMetHandle_;
         edm::Handle<reco::PFJetCollection> recoJetHandle_;
         edm::Handle<edm::TriggerResults> trigResultsHandle_;
         edm::Handle<trigger::TriggerEvent> trigEventHandle_;
-
+edm::Handle<bool> HBHENoiseFilterResultProducerHandle_;
+        edm::Handle<int> primaryVertexFilterHandle_;
+        edm::Handle<bool> globalSuperTightHalo2016FilterHandle_;
+        edm::Handle<bool> EcalDeadCellTriggerPrimitiveFilterHandle_;
+        edm::Handle<bool> ecalBadCalibFilterHandle_;
+        edm::Handle<bool> BadPFMuonFilterHandle_;
+        edm::Handle<bool> muonBadTrackFilterHandle_;
 
         std::string trigPath_;
         std::string trigPath0_;
@@ -106,15 +136,6 @@ class MetTrigSelfEffiForMuTrack : public edm::one::EDAnalyzer<edm::one::WatchRun
         std::vector<float> eta_;
         std::vector<float> phi_;
 
-//        std::vector<float> genPt_;
-//        std::vector<float> genEta_;
-//        std::vector<float> genPhi_;
-//        std::vector<float> genVxy_;
-//        std::vector<float> genVz_;
-//
-//        float genJetPt_;
-//        float genLeadMetPt_;
-//        float genSubLeadMetPt_;
         float caloPFMetPt_;
         float caloPFMetEta_;
         float caloPFMetPhi_;
@@ -134,8 +155,36 @@ class MetTrigSelfEffiForMuTrack : public edm::one::EDAnalyzer<edm::one::WatchRun
 	float chargedHadronEnergyFraction_;
 	float neutralHadronEnergyFraction_;
 	float recoPFMETJetDeltaPhi_;
+	float recoPFrecoilJetDeltaPhi_;
 	float recoil_;
+	float metmufrac_;
+	float metnomu_;
+	float musum_;
 
+        std::vector<float> genpt_;
+        std::vector<float> geneta_;
+        std::vector<float> genphi_;
+
+        float genPFMetPt_;
+        float genPFMetEta_;
+        float genPFMetPhi_;
+        float genJetPt_;
+        float genJetEta_;
+        float genJetPhi_;
+        float genJetPt1_;
+        float genJetEta1_;
+        float genJetPhi1_;
+        float genJetPt2_;
+        float genJetEta2_;
+        float genJetPhi2_;
+	float genchargedHadronEnergyFraction_;
+	float genneutralHadronEnergyFraction_;
+	float genPFMETJetDeltaPhi_;
+	float genPFrecoilJetDeltaPhi_;
+	float genrecoil_;
+        TTree *genTrackT_;
+
+	bool runGen = true;
         TTree *muTrackT_;
         TTree *genweight_;
 };
--- washAOD/scripts/runall.py
+++ washAOD/scripts/runall.py
@@ -2,7 +2,7 @@ import os, sys
 import subprocess
 
 
-test = True
+test = False
 if test:
 	mchis=['6p0']#['60p0']
 	dmchis=['2p0']#['20p0']
@@ -10,16 +10,16 @@ if test:
 	masslist = ["Mchi-%s_dMchi-%s"%(mchi,dmchi) for mchi,dmchi in zip(mchis,dmchis)]
 
 else:
-	#lifelist = [1,10,100,1000]
-	lifelist = [100]
-	#mchis=['52p5']
-	#dmchis=['5p0']
+	lifelist = [1,10,100,1000]
+	#lifelist = [100]
+	mchis=['52p5']
+	dmchis=['5p0']
 	#mchis=['5p25']
 	#dmchis=['0p5']
 	#mchis=['6p0']
 	#dmchis=['2p0']
-	mchis=['60p0']
-	dmchis=['20p0']
+	#mchis=['60p0']
+	#dmchis=['20p0']
 	#mchis=['6p0','60p0','5p25','52p5']
 	#dmchis=['2p0','20p0','0p5','5p0']
 	masslist = ["Mchi-%s_dMchi-%s"%(mchi,dmchi) for mchi,dmchi in zip(mchis,dmchis)]
@@ -33,7 +33,7 @@ makedir.wait()
 analyzer ='MetTrigSelfEffiForMuTrack_cfg.py'
 #analyzer ='iDMAnalyzer_cfg.py'
 #analyzer='MuRecoEffi_cfg.py'
-analyzer_name = {'MetTrigSelfEffiForMuTrack_cfg.py': 'MetTrigStudyv4',
+analyzer_name = {'MetTrigSelfEffiForMuTrack_cfg.py': 'MetTrigStudy_signal_gen_v8',
 		'iDMAnalyzer_cfg.py': 'iDMAnalysis',
 		'MuRecoEffi_cfg.py': 'MuRecoEffi'}
 for mass in masslist:
@@ -41,7 +41,8 @@ for mass in masslist:
 	#	odir = "/uscmst1b_scratch/lpc1/3DayLifetime/mireid/v3"
 		ofile= "{2}_{0}_ctau-{1}.root".format(mass,life,analyzer_name[analyzer])
 		if test:
-			cmdxx = "cmsRun ../python/{1} year=2018 inputFiles_load=../data/iDM/2018/signal/test.list outputFile=test_{0}".format(ofile,analyzer)#,mass,life)
+			cmdxx = "cmsRun ../python/{1} year=2018 inputFiles=/store/data/Run2018A/SingleMuon/AOD/17Sep2018-v2/90000/F8382E90-9BC4-4E4A-8EB2-952904BAE395.root outputFile=test_{0}".format(ofile,analyzer)#,mass,life)
+			#cmdxx = "cmsRun ../python/{1} year=2018 inputFiles_load=../data/iDM/2018/signal/test.list outputFile=test_{0}".format(ofile,analyzer)#,mass,life)
 		
 			print(cmdxx)
 			process1 = subprocess.Popen(cmdxx,shell=True, stdout=subprocess.PIPE)
@@ -50,7 +51,7 @@ for mass in masslist:
 			process1.stdout.close()
 			process1.wait()
 		else:
-			cmdxx = "cmsRun python/{4} year=2018 inputFiles_load=data/iDM/2018/signal/{2}_ctau-{3}.list outputFile={0}/{1}".format(odir,ofile,mass,life,analyzer)
+			cmdxx = "cmsRun ../python/{4} year=2018 inputFiles_load=../data/iDM/2018/signal/{2}_ctau-{3}.list outputFile={0}/{1}".format(odir,ofile,mass,life,analyzer)
 		
 			print(cmdxx)
 			process1 = subprocess.Popen(cmdxx,shell=True, stdout=subprocess.PIPE)
