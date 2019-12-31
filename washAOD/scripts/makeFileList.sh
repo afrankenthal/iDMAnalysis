0a37d7f055a96ebdc5d6c1f56742cf4c5477e1d3
Andre Frankenthal <as2872@cornell.edu>
2019-12-31 12:52:54 -0600

    Updated makeFileList.sh script

--- washAOD/scripts/makeFileList.sh
+++ washAOD/scripts/makeFileList.sh
@@ -1,24 +1,27 @@
-#! /bin/bash
+#!/bin/bash
 
-#### THIS SCRIPT NEEDS TO BE SOURCED!
-#### (due to use of eosls command)
+if [ "$#" -ne 4 ] && [ "$#" -ne 3 ]; then
+  echo "Usage: $0 year mchi dmchi [ctau]" >&2
+    exit 1
+fi
 
 #### Choose lpcuser and comment out the "input mode" 
 #### you don't want to use
 
-#lpcuser=as2872
-basedir="2018/signal"
+year=$1
+mchi=$2
+dmchi=$3
+if [ "$#" -eq 4 ]; then
+    ctaus=($4)
+else
+    ctaus=(1 10 100 1000)
+fi
 
-# Use single mass and ctau parameter
-#mchi=6p0
-#dmchi=2p0
-#ctau=10
-mchi=$1
-dmchi=$2
-ctau=$3
-dirs="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
+basedirEOS="$year/signal"
+basedirLOCAL="signal/$year"
 
 # OR use directories in EOS to get all samples (uncomment)
+#$lpcuser=as2872
 #dirs=$(eosls /store/user/$lpcuser/iDM/AOD_Samples/2018/GenFilter/)
 
 #for dir in $dirs; do 
@@ -28,19 +31,19 @@ dirs="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
 #    echo $mchi
 #done
 
-for dir in $dirs; do
+for ctau in "${ctaus[@]}"; do
+    dir="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
     echo "Processing $dir"
-    #eos root://cmseos.fnal.gov ls /store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir > data/iDM/$basedir/${dir}.list
-    eos root://cmseos.fnal.gov ls /store/group/lpcmetx/iDM/AOD/$basedir/$dir > ../data/iDM/$basedir/${dir}.list
-    sed -i '/MINIAOD/d' ../data/iDM/$basedir/${dir}.list
-    #sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir/|" data/iDM/$basedir/${dir}.list
-    sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/group/lpcmetx/iDM/AOD/$basedir/$dir/|" ../data/iDM/$basedir/${dir}.list
+    xrdfs root://cmseos.fnal.gov ls -u /store/group/lpcmetx/iDM/AOD/$basedirEOS/$dir > ../data/$basedirLOCAL/${dir}.list
+
+#    #eos root://cmseos.fnal.gov ls /store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir > data/iDM/$basedir/${dir}.list
+#    eos root://cmseos.fnal.gov ls /store/group/lpcmetx/iDM/AOD/$basedir/$dir > ../data/iDM/$basedir/${dir}.list
+#    sed -i '/MINIAOD/d' ../data/iDM/$basedir/${dir}.list
+#    #sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/$lpcuser/iDM/AOD_Samples/$basedir/$dir/|" data/iDM/$basedir/${dir}.list
+#    sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/group/lpcmetx/iDM/AOD/$basedir/$dir/|" ../data/iDM/$basedir/${dir}.list
 done
 
 #basename="Mchi-${mchi}_dMchi-${dmchi}_ctau-${ctau}"
-#
 #eos root://cmseos.fnal.gov ls /store/user/as2872/iDM/AOD_Samples/$basedir/$basename > data/iDM/$basedir/${basename}.list
-#
 #sed -i '/MINIAOD/d' data/iDM/$basedir/${basename}.list
-#
 #sed -i "s|^|root://cmsxrootd-site.fnal.gov//store/user/asterenb/iDM/AOD_Samples/$basedir/$basename/|" data/iDM/$basedir/${basename}.list
