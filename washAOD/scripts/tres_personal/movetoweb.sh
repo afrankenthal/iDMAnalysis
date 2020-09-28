c6312ec22c49dc0d4e07073718f895498e51d81f
tres <tres.reid@gmail.com>
2020-09-28 07:52:43 -0500

    pre andre merge

--- washAOD/scripts/tres_personal/movetoweb.sh
+++ washAOD/scripts/tres_personal/movetoweb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dir=/uscms/home/mreid/nobackup/CRAB/signal_region_analysis/CMSSW_10_2_18/src/iDMSkimmer/washAOD
+#dir=/uscms/home/mreid/nobackup/CRAB/signal_region_analysis/CMSSW_10_2_18/src/iDMSkimmer/washAOD
+dir=/uscms/home/mreid/nobackup/CRAB/signal_region_analysis/CMSSW_10_2_18/src/skimmer_push/iDMSkimmer/washAOD
 for d in $(find ${dir}/macros/plots/ -type d)
 do
   #Do something, the directory is accessible with $d:
@@ -8,9 +9,9 @@ do
   python $dir/scripts/make_html_listing.py $d
 done
 #cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/moresamples/
-cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/alltrigs/
+#cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/alltrigs/
 #cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/tanh_fit/
 #cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/erf_fit/
 #cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/trigstudy/
-#cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/macrorun/
+cp -r $dir/macros/plots/* /publicweb/m/mreid/iDM_AN_Plots/dsaID/
 
